4563e459912a0b9a8c7c02918015bb610bd89f82
yugthapa015 <112218953+yugthapa015@users.noreply.github.com>
2022-12-01 10:43:41 -0600

    Add files via upload

---
+++ app_prog_project/ourgazebonode.py
@@ -0,0 +1,131 @@
+
+#!/usr/bin/env python3
+
+import rospy
+from geometry_msgs.msg import Twist, Point, Quaternion
+import tf
+from math import radians, copysign, sqrt, pow, pi, atan2
+from tf.transformations import euler_from_quaternion
+import numpy as np
+
+
+
+class GotoPoint():
+    def __init__(self,goal_x,goal_y,goal_z):
+        rospy.init_node('turtlebot3_pointop_key', anonymous=False)
+        rospy.on_shutdown(self.shutdown)
+        self.cmd_vel = rospy.Publisher('cmd_vel', Twist, queue_size=5)
+        position = Point()
+        move_cmd = Twist()
+        r = rospy.Rate(10)
+        self.tf_listener = tf.TransformListener()
+        self.odom_frame = 'odom'
+        self.goal_x =float(goal_x)
+        self.goal_y = float(goal_y)
+        self.goal_z = float(goal_z)
+
+        try:
+            self.tf_listener.waitForTransform(self.odom_frame, 'base_footprint', rospy.Time(), rospy.Duration(1.0))
+            self.base_frame = 'base_footprint'
+        except (tf.Exception, tf.ConnectivityException, tf.LookupException):
+            try:
+                self.tf_listener.waitForTransform(self.odom_frame, 'base_link', rospy.Time(), rospy.Duration(1.0))
+                self.base_frame = 'base_link'
+            except (tf.Exception, tf.ConnectivityException, tf.LookupException):
+                rospy.loginfo("Cannot find transform between odom and base_link or base_footprint")
+                rospy.signal_shutdown("tf Exception")
+
+        (position, rotation) = self.get_odom()
+        last_rotation = 0
+        linear_speed = 1
+        angular_speed = 1
+        
+        if self.goal_z > 180 or self.goal_z < -180:
+            print("you input wrong z range.")
+            self.shutdown()
+        self.goal_z = np.deg2rad(self.goal_z)
+        goal_distance = sqrt(pow(self.goal_x - position.x, 2) + pow(self.goal_y - position.y, 2))
+        distance = goal_distance
+
+        while distance > 0.05:
+            (position, rotation) = self.get_odom()
+            x_start = position.x
+            y_start = position.y
+            path_angle = atan2(self.goal_y - y_start, self.goal_x- x_start)
+
+            if path_angle < -pi/4 or path_angle > pi/4:
+                if self.goal_y < 0 and y_start < self.goal_y:
+                    path_angle = -2*pi + path_angle
+                elif self.goal_y >= 0 and y_start > self.goal_y:
+                    path_angle = 2*pi + path_angle
+            if last_rotation > pi-0.1 and rotation <= 0:
+                rotation = 2*pi + rotation
+            elif last_rotation < -pi+0.1 and rotation > 0:
+                rotation = -2*pi + rotation
+            move_cmd.angular.z = angular_speed * path_angle-rotation
+
+            distance = sqrt(pow((self.goal_x - x_start), 2) + pow((self.goal_y - y_start), 2))
+            move_cmd.linear.x = min(linear_speed * distance, 0.1)
+
+            if move_cmd.angular.z > 0:
+                move_cmd.angular.z = min(move_cmd.angular.z, 1.5)
+            else:
+                move_cmd.angular.z = max(move_cmd.angular.z, -1.5)
+
+            last_rotation = rotation
+            self.cmd_vel.publish(move_cmd)
+            r.sleep()
+        (position, rotation) = self.get_odom()
+
+        while abs(rotation - self.goal_z) > 0.05:
+            (position, rotation) = self.get_odom()
+            if self.goal_z >= 0:
+                if rotation <= self.goal_z and rotation >= self.goal_z - pi:
+                    move_cmd.linear.x = 0.00
+                    move_cmd.angular.z = 0.5
+                else:
+                    move_cmd.linear.x = 0.00
+                    move_cmd.angular.z = -0.5
+            else:
+                if rotation <= self.goal_z + pi and rotation > self.goal_z:
+                    move_cmd.linear.x = 0.00
+                    move_cmd.angular.z = -0.5
+                else:
+                    move_cmd.linear.x = 0.00
+                    move_cmd.angular.z = 0.5
+            self.cmd_vel.publish(move_cmd)
+            r.sleep()
+
+
+        rospy.loginfo("Stopping the robot...")
+        self.cmd_vel.publish(Twist())
+
+
+    def get_odom(self):
+        try:
+            (trans, rot) = self.tf_listener.lookupTransform(self.odom_frame, self.base_frame, rospy.Time(0))
+            rotation = euler_from_quaternion(rot)
+
+        except (tf.Exception, tf.ConnectivityException, tf.LookupException):
+            rospy.loginfo("TF Exception")
+            return
+
+        return (Point(*trans), rotation[2])
+
+
+    def shutdown(self):
+        self.cmd_vel.publish(Twist())
+        rospy.sleep(1)
+
+
+if __name__ == '__main__':
+    try:
+        while not rospy.is_shutdown():
+            xpoints = [1,1,2,2]
+            ypoints = [0,1,1,2]
+            angles = [90,0,90,0]
+            for i in range(len(xpoints)):
+                GotoPoint(xpoints[i],ypoints[i],angles[i])
+            GotoPoint.shutdowm()
+    except:
+        rospy.loginfo("shutdown program.") 
